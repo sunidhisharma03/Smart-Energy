635527dccde7057af8ceddb0af51a88c2ef4efa1
Sunidhi Sharma <sunidhisharma3002@gmail.com>
2024-11-06 20:57:47 +0545

    First commit

---
+++ app/_layout.jsx
@@ -0,0 +1,13 @@
+import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { Stack } from 'expo-router';
+
+const RooyLayout = () => {
+  return (
+    <Stack>
+      <Stack.Screen name="index" options={{ headerShown: false }} />
+    </Stack>
+  );
+};
+
+export default RooyLayout;
